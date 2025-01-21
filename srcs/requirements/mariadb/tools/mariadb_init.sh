711418b2e9e16cd96c4d5f32615fc3f6d6c45e09
Yuji Toshihiro <ytoshihi@3-H-6.42heilbronn.de>
2025-01-21 13:50:55 +0100

    modify comments

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Create database and user, set a password, change root password, grant privileges
-if [ ! -d "/var/lib/mysql/${WP_DB_NAME}" ]; then
+# Create database and setup
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
 	mariadbd --user=mysql --bootstrap --console <<EOSQL
 USE mysql;
 FLUSH PRIVILEGES;
-CREATE DATABASE ${WP_DB_NAME};
-CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
+CREATE DATABASE ${DB_NAME};
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_DB_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOSQL
 else
--- srcs/requirements/wordpress/tools/wordpress_init.sh
+++ srcs/requirements/wordpress/tools/wordpress_init.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-# If there is no wp-config.php file, create it by copying wp-config-sample.php
+
+# If there is no wp-config.php file, create it
 if [ ! -f "wp-config.php" ]; then 
 	mv wp-config-sample.php wp-config.php
 fi
 
-# Setup credentials in wp-config.php
-sed -i "s|define( 'DB_NAME', 'database_name_here' );|define( 'DB_NAME', '"${WP_DB_NAME}"' );|g" \
+# change the database settings with the environment variables
+sed -i "s|define( 'DB_NAME', 'database_name_here' );|define( 'DB_NAME', '"${DB_NAME}"' );|g" \
 	wp-config.php && \
-sed -i "s|define( 'DB_USER', 'username_here' );|define( 'DB_USER', '"${WP_DB_USER}"' );|g" \
+sed -i "s|define( 'DB_USER', 'username_here' );|define( 'DB_USER', '"${DB_USER}"' );|g" \
 	wp-config.php && \
-sed -i "s|define( 'DB_PASSWORD', 'password_here' );|define( 'DB_PASSWORD', '"${WP_DB_PASSWORD}"' );|g" \
+sed -i "s|define( 'DB_PASSWORD', 'password_here' );|define( 'DB_PASSWORD', '"${DB_PASSWORD}"' );|g" \
 	wp-config.php
 
-# Setup database host in wp-config.php
 sed -i -r "s/localhost/${WP_DB_HOST}/1" wp-config.php
 
-# Install Wordpress with wp-cli
+// Install WordPress and initialize it with environment variables
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 php82 wp-cli.phar core install \
---url="${INCEPTION_URL}" --title="Inception" \
+--url="${DOMAIN_NAME}" --title="Inception" \
 --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" \
 --admin_email="${WP_ADMIN_EMAIL}"
 
-# Start php-fpm
+# implement PHP-FPM
 /usr/sbin/php-fpm82 -F
